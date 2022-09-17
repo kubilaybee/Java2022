82061b1b3059ccde7c4187129dad6928e2b915ef
kubil <kubilaybee@gmail.com>
2022-09-17 16:53:51 +0300

    add variables

---
+++ src/Variables/Main.java
@@ -0,0 +1,9 @@
+package Variables;
+
+public class Main {
+    public static void main(String[] args){
+        int studentCounter = 10;
+        String message = "Hello Again !";
+        System.out.println(studentCounter + " number "+ message);
+    }
+}
