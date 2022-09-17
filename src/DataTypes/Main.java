a1b1c88f1dc391dfed6ea8af4ef36a7ce06b5410
kubil <kubilaybee@gmail.com>
2022-09-17 18:50:40 +0300

    add dataTypes

---
+++ src/DataTypes/Main.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class Main {
+    public static void main(String[] args){
+        int number = 12;
+        number = 13;
+        String cityName = "ISTANBUL";
+
+        // ascii
+        char character = 'A';
+
+        boolean isTrue = false;
+
+    }
+}
