2f517c40371954f4154df1fb20571b141c985a68
kubil <kubilaybee@gmail.com>
2022-09-17 18:54:14 +0300

    add conditionals

---
+++ src/Conditionals/Main.java
@@ -0,0 +1,15 @@
+package Conditionals;
+
+public class Main {
+    public static void main(String[] args){
+        // conditionals
+        int number = 20;
+        if (number<20){
+            System.out.println("20den kucuk");
+        } else if (20==number) {
+            System.out.println("Sayı 20ye esit");
+        } else {
+            System.out.println("20den buyuk");
+        }
+    }
+}
