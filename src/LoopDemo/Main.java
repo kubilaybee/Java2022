d1f717c27ebe14e50fcd5c982aad61bcbeef51bf
kubil <kubilaybee@gmail.com>
2022-09-18 21:07:36 +0300

    add forLoop

---
+++ src/LoopDemo/Main.java
@@ -0,0 +1,10 @@
+package LoopDemo;
+
+public class Main {
+    public static void main(String[] args){
+        // For Loop
+        for (int i = 0; i<7;i++){
+            System.out.println(i);
+        }
+    }
+}
