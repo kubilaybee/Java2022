af2826d596d2730900edaf5e28c9581443a95f05
kubil <kubilaybee@gmail.com>
2022-09-19 01:38:35 +0300

    add stringDemo

---
+++ src/StringDemo/Main.java
@@ -0,0 +1,20 @@
+package StringDemo;
+
+public class Main {
+    public static void main(String[] args){
+        // String
+        String dailyMessage = "The weather is so beatiful on today..";
+        System.out.println(dailyMessage);
+
+        System.out.println("Text Size=>"+dailyMessage.length());
+        System.out.println("5. Char is=>"+dailyMessage.charAt(5));
+        System.out.println(dailyMessage.concat("Hello Again !!"));
+        System.out.println(dailyMessage.startsWith("K"));
+        System.out.println(dailyMessage.endsWith("."));
+        char[] chars = new char[5];
+        dailyMessage.getChars(0,5,chars,0);
+        System.out.println(chars);
+        System.out.println(dailyMessage.indexOf("is"));
+        System.out.println(dailyMessage.lastIndexOf("so"));
+    }
+}
