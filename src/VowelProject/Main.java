923bdc28dab9dac7026f9fbe63449a829d4c0159
kubil <kubilaybee@gmail.com>
2022-09-19 14:52:30 +0300

    complete vowelProject

--- src/VowelProject/Main.java
+++ src/VowelProject/Main.java
@@ -1,8 +1,21 @@
 package VowelProject;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
         // vowel project
+        String charWord = "E";
+        ArrayList<String> thickVowels= new ArrayList<String>();
+        thickVowels.add("a");
+        thickVowels.add("ı");
+        thickVowels.add("o");
+        thickVowels.add("u");
+
+        if (thickVowels.contains(charWord.toLowerCase())){
+            System.out.println("Thick Vowels");
+        } else {
+            System.out.println("Thin Vowels");
+        }
 
     }
 }
