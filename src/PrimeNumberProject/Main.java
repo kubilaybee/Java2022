130ecc2b128cb90b67d53d7c0e9834243c8acb48
kubil <kubilaybee@gmail.com>
2022-09-19 14:39:53 +0300

    update primeProject and add vowelProject

--- src/PrimeNumberProject/Main.java
+++ src/PrimeNumberProject/Main.java
@@ -5,16 +5,20 @@ public class Main {
         // prime number project
         int number = 9;
         boolean isPrime = true;
-        for (int i = 3;i<number;i++){
-            if (number%i==0){
-                isPrime=false;
+        if (0<number){
+            for (int i = 3;i<number;i++){
+                if (number%i==0){
+                    isPrime=false;
+                }
+            }
+            if (isPrime){
+                System.out.println("The number is PRIME");
+            } else {
+                System.out.println("The number is not prime");
             }
         }
-        if (isPrime){
-            System.out.println("The number is PRIME");
-        } else {
-            System.out.println("The number is not prime");
-        }
+        System.out.println("The number is not prime");
+
 
     }
 }
---
+++ src/VowelProject/Main.java
@@ -0,0 +1,8 @@
+package VowelProject;
+
+public class Main {
+    public static void main(String[] args){
+        // vowel project
+
+    }
+}
