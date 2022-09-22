35efefdf9094a38b6fabbf4c1c377aa115b141df
kubil <kubilaybee@gmail.com>
2022-09-22 22:01:41 +0300

    complete 29.vid

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -10,5 +10,19 @@ public class Main {
         customerManager.addCustomer();
         customerManager.updateCustomer();
         customerManager.deleteCustomer();
+
+        // valueType
+        int number1 = 10;
+        int number2 = 20;
+        number2=number1;
+        number1 = 30;
+        System.out.println(number2);
+
+        // referenceType
+        int[] numbers1 = new int[]{1,2,3};
+        int[] numbers2 = new int[]{4,8,9};
+        numbers2 = numbers1;
+        numbers1[0] = 10;
+        System.out.println(numbers2[0]);
     }
 }
