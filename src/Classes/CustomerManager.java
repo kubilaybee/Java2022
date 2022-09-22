a72738a5051d79f9bfdc717666994a6a652136ed
kubil <kubilaybee@gmail.com>
2022-09-22 19:00:21 +0300

    complete 27.vid

---
+++ src/Classes/CustomerManager.java
@@ -0,0 +1,14 @@
+package Classes;
+
+public class CustomerManager {
+
+    public void addCustomer(){
+        System.out.println("Customer Added!!");
+    }
+    public void deleteCustomer(){
+        System.out.println("Customer Deleted!!");
+    }
+    public void updateCustomer(){
+        System.out.println("Customer Updated!!");
+    }
+}
---
+++ src/Classes/Main.java
@@ -0,0 +1,11 @@
+package Classes;
+
+public class Main {
+    public static void main(String[] args){
+        // Classes
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.addCustomer();
+        customerManager.updateCustomer();
+        customerManager.deleteCustomer();
+    }
+}
