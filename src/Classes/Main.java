8c3383045b65b9c3527b1e1c2d943d05a842d24f
kubil <kubilaybee@gmail.com>
2022-09-22 21:45:08 +0300

    complete 28.vid

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -3,7 +3,10 @@ package Classes;
 public class Main {
     public static void main(String[] args){
         // Classes
+        // referenceType ->stack ->heap -> garbageCollector
         CustomerManager customerManager = new CustomerManager();
+        CustomerManager customerManager2 = new CustomerManager();
+        customerManager2 = customerManager;
         customerManager.addCustomer();
         customerManager.updateCustomer();
         customerManager.deleteCustomer();
