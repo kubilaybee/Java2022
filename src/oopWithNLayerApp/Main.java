23e09f5b9b5a186310f16d2300a165d70e9d32ee
kubil <kubilaybee@gmail.com>
2022-10-05 21:48:17 +0300

    Java-3.1

---
+++ src/oopWithNLayerApp/Main.java
@@ -0,0 +1,7 @@
+package oopWithNLayerApp;
+
+public class Main {
+    public static void main(String[] args){
+        // java 3
+    }
+}
---
+++ src/oopWithNLayerApp/dataAccess/jdbcProductDao.java
@@ -0,0 +1,4 @@
+package oopWithNLayerApp.dataAccess;
+
+public class jdbcProductDao {
+}
